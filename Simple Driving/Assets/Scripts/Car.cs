c9d78588bc35d0aa509dbe4f01574684dce392a1
Kshitijv24 <kshitijv24@gmail.com>
2022-11-13 12:25:22 +0530

    Loading a demo scene if our car collide with an obstacle

--- Simple Driving/Assets/Scripts/Car.cs
+++ Simple Driving/Assets/Scripts/Car.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
@@ -20,5 +21,13 @@ public class Car : MonoBehaviour
     public void Steer(int value)
     {
         steerValue = value;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
