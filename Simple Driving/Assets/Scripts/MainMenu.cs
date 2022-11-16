ac1dd1feb123edaad9caa0ab20fd7da25318f42c
Kshitijv24 <kshitijv24@gmail.com>
2022-11-16 22:05:11 +0530

    added energy system in our game so when player play the game it cost 1 energy point from the max 5 energy points when all the energy points are used by the player then the player have to wait for 1 minute to get all the 5 energy points back

--- Simple Driving/Assets/Scripts/MainMenu.cs
+++ Simple Driving/Assets/Scripts/MainMenu.cs
@@ -6,14 +6,14 @@ using System;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI highScoreText;
+    [SerializeField] TextMeshProUGUI energyText;
     [SerializeField] int maxEnergy;
-    [SerializeField] int energyRechargeDuration;
+    [SerializeField] int energyRechargeDurationInMinutes;
 
     const string EnergyKey = "Energy";
     const string EnergyReadyKey = "EnergyReady";
 
     int energy = 0;
-
 
     private void Start()
     {
@@ -23,12 +23,35 @@ public class MainMenu : MonoBehaviour
 
         if(energy == 0)
         {
-            
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
+            if(energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if(DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
         }
+
+        energyText.text = "Energy Left: " + energy;
     }
 
     public void Play()
     {
+        if(energy < 1) { return; }
+
+        energy--;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if(energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDurationInMinutes);
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+        }
+
         SceneManager.LoadScene(1);
     }
 }
